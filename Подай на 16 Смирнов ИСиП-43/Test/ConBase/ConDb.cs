0d2930c5c0940bb02fd985cf5410755989fd9767
meowMari <mariconqueror@gmail.com>
2023-02-06 16:24:10 +0300

    DemoEx

---
+++ Подай на 16 Смирнов ИСиП-43/Test/ConBase/ConDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Test.Base;
+
+namespace Test.ConBase
+{
+    internal class ConDb
+    {
+        public static Frame mainFrame { get; set; }
+        
+        
+            public static DBaseEntities db = new DBaseEntities();
+        
+    }
+}
---
+++ Подай на 16 Смирнов ИСиП-43/Test/Pageses/ServiceListPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Test.ConBase;
+
+namespace Test.Pageses
+{
+    /// <summary>
+    /// Логика взаимодействия для ServiceListPage.xaml
+    /// </summary>
+    public partial class ServiceListPage : Page
+    {
+        public ServiceListPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                //вывод данных
+                ProductList.ItemsSource = ConDb.db.ServicePrice.Take(100).ToList();
+                ResultTxb.Text = ProductList.Items.Count + "/" + ConDb.db.ServicePrice.Count().ToString();
+
+
+            }
+            catch (Exception except)
+
+            {
+                MessageBox.Show(except.Message, "Eror", MessageBoxButton.OK);
+
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                ProductList.ItemsSource = ConDb.db.ServicePrice.Where(x => x.Service.Contains(TxbSearch.Text)).Take(15).ToList();
+                ResultTxb.Text = ProductList.Items.Count + "/" + ConDb.db.ServicePrice.Where(x => x.Service.Contains(TxbSearch.Text)).Count().ToString();
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+
+        }
+    }
+}
